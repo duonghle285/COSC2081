e243270f3bdeda522201fc215373b54b658df319
gnoud9x <jerryfrz@gmail.com>
2022-12-14 15:31:35 +0700

    First commit

--- src/Week_8.java
+++ src/Week_8.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class Week_8 {
+import java.io.*;
+import java.util.Scanner;
+
+public class Week_8 {
+    public static void main(String[] args) {
+        PrintWriter pw = null;
+        System.out.println("Please input your name, address and age in order: ");
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        String address = scanner.nextLine();
+        int age = scanner.nextInt();
+        try {
+            pw = new PrintWriter(new FileWriter("users.txt", true));
+            pw.println(name + "\n" + address + "\n" + age);
+            pw.close();
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
 }
